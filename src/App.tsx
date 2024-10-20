5d87aff2af8c97753e8a15c46f1a06f01aa31423
Jeffer <manymartinez83@gmail.com>
2024-10-19 23:06:29 -0600

    feature: add CalorieTrackerFunction and storage in localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useReducer } from "react"
+import { useReducer, useEffect, useMemo } from "react"
 import { activityReducer,initialState } from "./reducers/activity-reducer"
 import Form from "./components/Form"
 import ActivityList from "./components/ActivityList"
@@ -6,17 +6,24 @@ import { ChakraProvider } from '@chakra-ui/react'
 import {
   useDisclosure,
 } from '@chakra-ui/react'
+import CalorieTracker from "./components/CalorieTracker"
 
 
 function App() {
  const[state, dispatch]= useReducer(activityReducer,initialState)
  const { isOpen, onOpen, onClose } = useDisclosure()
 
+ useEffect(()=>{
+  localStorage.setItem('activities', JSON.stringify(state.activities))
+ },[state.activities])
+
+ const CanRestartApp=()=>useMemo(()=> state.activities.length > 0,[state.activities])
+
   return (
     <>
     <ChakraProvider>
         <header className="bg-purple-800 py-3">
-          <div className=" max-w-4xl mx-auto flex justify-between">
+          <div className=" max-w-4xl mx-auto flex justify-between items-center">
             <ActivityList
               activities={state.activities}
               dispatch={dispatch}
@@ -27,6 +34,13 @@ function App() {
             <h1 className="text-center text-lg font-bold text-white uppercase">
               Contador de Calorias
             </h1>
+
+            <button className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-lg text-sm disabled:opacity-10"
+              disabled={!CanRestartApp()}
+              onClick={()=>dispatch({type:'restart-app'})}
+            >
+              Reiniciar App
+            </button>
             <div>
 
             </div>
@@ -41,6 +55,14 @@ function App() {
               isOpen={isOpen}
               onClose={onClose}
               onOpen={onOpen}
+            />
+          </div>
+        </section>
+
+        <section className="bg-gray-800 p-10">
+          <div className="max-w-4xl mx-auto">
+            <CalorieTracker 
+              activities={state.activities}
             />
           </div>
         </section>
--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -3,14 +3,21 @@ import { Activity } from "../types"
 export type ActivityActions= 
 { type: 'save-activity', payload: {newActivity: Activity} } |
 { type: 'edit-activity', payload: {id: Activity['id']} }|
-{ type: 'delete-activity', payload: {id: Activity['id']} }
+{ type: 'delete-activity', payload: {id: Activity['id']} } |
+{ type: 'restart-app'}
 
 export type ActivityState = {
     activities: Activity[],
     activeId: Activity['id']
 }
+
+const localStorageActivities= (): Activity[]=>{
+    const activities= localStorage.getItem('activities')
+    return activities ? JSON.parse(activities) : []
+}
+
 export const initialState: ActivityState= {
-    activities: [],
+    activities: localStorageActivities(),
     activeId: ''
 }
 
@@ -43,6 +50,13 @@ export const activityReducer = (
         return{
             ...state,
             activities:state.activities.filter(activity => activity.id!==action.payload.id)
+        }
+    }
+
+    if(action.type === 'restart-app'){
+        return{
+            activities: [],
+            activeId: ''
         }
     }
 
